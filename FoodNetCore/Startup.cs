2c0b9df9d582f14d2919d7e9a367b97f7b45a94b
saxero <jvives@gmail.com>
2017-12-28 14:42:52 +0100

    Adding a configuration source. appSettings json

--- FoodNetCore/Startup.cs
+++ FoodNetCore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace FoodNetCore
 {
@@ -18,7 +19,9 @@ namespace FoodNetCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env, 
+                              IConfiguration configuration)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +30,8 @@ namespace FoodNetCore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World Core!");
+                var greeting = configuration["Greeting"];
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
